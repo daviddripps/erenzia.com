a61321db6d1ac04712a8908d5129347ac91e723b
David Dripps <david.dripps@gmail.com>
2023-03-18 16:14:15 -0400

    feat(ui): add Material UI and server-side rendering
    Followed the official example here:
    https://github.com/mui/material-ui/tree/master/examples/material-next-ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,30 @@
-import type { AppProps } from 'next/app'
+import * as React from 'react'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from '~/ui/theme'
+import createEmotionCache from '~/utils/createEmotionCache'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
